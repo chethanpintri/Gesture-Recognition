4a77cc921bfbd57fa1715e273d1d75ca84aae1f2
chethan pintri <chethan pintri>
2017-05-07 19:33:12 +0530

    First commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(544, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.openAllToolStripMenuItem,
+            this.multiToolStripMenuItem,
+            this.trainToolStripMenuItem,
+            this.resultToolStripMenuItem});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.openToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem1.Text = "Test";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // openAllToolStripMenuItem
+            // 
+            this.openAllToolStripMenuItem.Name = "openAllToolStripMenuItem";
+            this.openAllToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openAllToolStripMenuItem.Text = "Open all";
+            this.openAllToolStripMenuItem.Click += new System.EventHandler(this.openAllToolStripMenuItem_Click);
+            // 
+            // multiToolStripMenuItem
+            // 
+            this.multiToolStripMenuItem.Name = "multiToolStripMenuItem";
+            this.multiToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.multiToolStripMenuItem.Text = "multi";
+            this.multiToolStripMenuItem.Click += new System.EventHandler(this.multiToolStripMenuItem_Click);
+            // 
+            // trainToolStripMenuItem
+            // 
+            this.trainToolStripMenuItem.Name = "trainToolStripMenuItem";
+            this.trainToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.trainToolStripMenuItem.Text = "Train";
+            this.trainToolStripMenuItem.Click += new System.EventHandler(this.trainToolStripMenuItem_Click);
+            // 
+            // resultToolStripMenuItem
+            // 
+            this.resultToolStripMenuItem.Name = "resultToolStripMenuItem";
+            this.resultToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.resultToolStripMenuItem.Text = "Result";
+            this.resultToolStripMenuItem.Click += new System.EventHandler(this.resultToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(544, 418);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 205);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(274, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 205);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(2, 211);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(268, 205);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(274, 211);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(268, 205);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 442);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ToolStripMenuItem openAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resultToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging.ComplexFilters;
+using AForge.Imaging.Filters;
+using SVM;
+
+
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+using AForge.Imaging.Textures;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        List<Bitmap> images;
+        List<float[]> featuresofallimages;
+        List<float[]> featuresofalltest;
+        int count = 0;
+        FileInfo[] Images;
+        Model model;
+        Bitmap im;
+        Bitmap originalImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Bitmap> LoadImages(String folderpath)
+        {
+
+            String[] imagePath = Directory.GetFiles(folderpath, "*.*");
+            images = new List<Bitmap>();
+            foreach (var path in imagePath)
+            {
+                count++;
+                Bitmap image = new Bitmap(path);
+                images.Add(image);
+            }
+            return images;
+        }
+        public void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            featurefortest();
+
+        }
+        public void featurefortest()
+        {
+            string filepath = @"F:test.txt";
+            List<int> dummy = new List<int>(0);
+            OpenFileDialog openDialog = new OpenFileDialog();
+            var returnValue = openDialog.ShowDialog();
+            if (returnValue == DialogResult.OK)
+            {
+                List<float> featuresfortestcase = new List<float>();
+                featuresofalltest = new List<float[]>();
+                List<float[]> ftcase = new List<float[]>();
+                originalImage = new Bitmap(openDialog.FileName);
+                 featuresfortestcase = ExtractFeaturOfOneImage(originalImage);
+                featuresofalltest.Add(featuresfortestcase.ToArray());
+                FileStream fst = new FileStream(filepath, FileMode.Create, FileAccess.Write);
+                StreamWriter swt = new StreamWriter(fst);
+                for (int i = 0; i < featuresofalltest.Count; i++)
+                {
+                    int c = 0;
+                    swt.Write(0 + " ");
+                    foreach (var value in featuresofalltest[i])
+                    {
+                        swt.Write(c + 1 + ":" + value + " ");
+                        c++;
+                    }
+                    swt.WriteLine();
+
+                }
+                swt.Flush();
+                swt.Close();
+                fst.Close();
+
+            }
+            
+
+        }
+        void WriteToFile(String filepath, List<float[]> features, List<int> labels)
+        {
+            FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < features.Count; i++)
+            {
+                int c = 0;
+                sw.Write(labels[i] + " ");
+                foreach (var value in features[i])
+                {
+                    sw.Write(c + 1 + ":" + value + " ");
+                    c++;
+                }
+                sw.WriteLine();
+
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        public void finalfeature()
+        {
+            String[] lookuptable = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+            String partialpath = @"F:\Gesture\";
+            List<float[]> finalfeatures = new List<float[]>();
+            List<int> labels = new List<int>();
+            for (int i = 0; i < lookuptable.Length; i++)
+            {
+                String Fullpath = partialpath + lookuptable[i];
+                List<Bitmap> rawimages = LoadImages(Fullpath);
+                List<float[]> features = ExtractFeaturesOfAllImages(rawimages);
+                finalfeatures.AddRange(features);
+                for (int j = 0; j < features.Count; j++)
+                {
+                    labels.Add(i + 1);
+                }
+            }
+            MessageBox.Show("done");
+            WriteToFile(@"F:train.txt", finalfeatures, labels);
+        }
+        public Byte max(Byte a, Byte b, Byte c)
+        {
+            if (a > b && a > c) return a;
+            else if (b > a && b > c) return b;
+            else return c;
+        }
+        public Byte min(Byte a, Byte b, Byte c)
+        {
+            if (a < b && a < c) return a;
+            else if (b < a && b < c) return b;
+            else return c;
+        }
+        public Bitmap ResizeBitmap(Bitmap b, int nWidth, int nHeight)
+        {
+            Bitmap result = new Bitmap(nWidth, nHeight);
+            using (Graphics g = Graphics.FromImage((Bitmap)result)) g.DrawImage(b, 0, 0, nWidth, nHeight);
+            return result;
+        }
+        public List<float> ExtractFeaturOfOneImage(Bitmap image1)
+        {
+            im = (Bitmap)image1.Clone();
+            detect();
+
+
+            /* for (int x = 0; x < im.Width; x++)
+             {
+                 for (int y = 0; y < im.Height; y++)
+                 {
+                     Color pixelColor = im.GetPixel(x, y);
+                     Byte r = pixelColor.R;
+                     Byte g = pixelColor.G;
+                     Byte b = pixelColor.B;
+                     if (r > 95 && g > 40 && b > 20 && (max(r, g, b) - min(r, g, b)) > 15 && Math.Abs(r - g) > 15 && r > g && r > b)
+                     {
+                         im.SetPixel(x, y, Color.White);
+
+                     }
+                     else
+                         im.SetPixel(x, y, Color.Black);
+                 }
+             }*/
+            Bitmap resizeim = ResizeBitmap(im, 500, 500);
+            GrayscaleBT709 grayobj = new GrayscaleBT709();
+            Bitmap imagegray = grayobj.Apply(resizeim);
+            Threshold obj = new Threshold(100);
+            Bitmap im1 = obj.Apply(imagegray);
+            Closing cobj = new Closing();
+            Bitmap im2 = cobj.Apply(im1);
+            Opening oobj = new Opening();
+            Bitmap im3 = oobj.Apply(im2);
+            ExtractBiggestBlob eobj = new ExtractBiggestBlob();
+            Bitmap eblob = eobj.Apply(im3);
+            AForge.IntPoint a = eobj.BlobPosition;
+            pictureBox3.Image = eblob;
+            Bitmap im4 = (Bitmap)im.Clone();
+            im4 = ResizeBitmap(im4, 500, 500);
+            for (int i = 0; i < eblob.Width; i++)
+            {
+
+                for (int j = 0; j < eblob.Height; j++)
+                {
+                    int x = i + a.X;
+                    int y = j + a.Y;
+                    Color pixelcolor1 = eblob.GetPixel(i, j);
+                    Byte r = pixelcolor1.R;
+                    Byte g = pixelcolor1.G;
+                    Byte b = pixelcolor1.B;
+
+                    if (r == 255 && g == 255 && b == 255)
+                        continue;
+                    else
+                        im4.SetPixel(x, y, Color.Black);
+                }
+
+            }
+            for (int i = 0; i < eblob.Width; i++)
+            {
+
+                for (int j = 0; j < eblob.Height; j++)
+                {
+                    if ((i <= a.X || i >= eblob.Width + a.X) || (j <= a.Y || j >= eblob.Height + a.Y))
+                    {
+
+                        im4.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+            GrayscaleBT709 objgray = new GrayscaleBT709();
+            Bitmap grayim = objgray.Apply(im4);
+            CannyEdgeDetector cannydetector = new CannyEdgeDetector();
+            Bitmap cannyim = cannydetector.Apply(grayim);
+            pictureBox2.Image = cannyim;
+            int sum = 0;
+            pictureBox4.Image = im4;
+            int p = cannyim.Width / 6;
+            int q = cannyim.Height / 6;
+            List<int> featureVector = new List<int>();
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 6; j++)
+                {
+                    count = 0;
+                    for (int x = i * p; x < ((i * p) + p); x++)
+                        for (int y = j * q; y < ((j * q) + q); y++)
+                        {
+                            Color pixelcolor1 = cannyim.GetPixel(x, y);
+
+                            if (pixelcolor1.R != 0 && pixelcolor1.G != 0 && pixelcolor1.B != 0)
+                             
+                                count++;
+                        }
+                    sum += count;
+                    featureVector.Add(count);
+                    String str1 = Convert.ToString(featureVector[j + i]);
+                    //  MessageBox.Show(str1);
+
+                }
+            // MessageBox.Show("done");
+            String str4 = Convert.ToString(sum);
+            //  MessageBox.Show(str4);
+            List<float> per = new List<float>();
+            for (int i = 0; i < 36; i++)
+            {
+                float gg = (float)(featureVector[i] / (sum * 1.0));
+                per.Add(gg);
+                String str3 = Convert.ToString(gg);
+
+                // MessageBox.Show(str3);
+
+            }
+            return per;
+        }
+        public List<float[]> ExtractFeaturesOfAllImages(List<Bitmap> images)
+        {
+            featuresofallimages = new List<float[]>();
+            List<float> featuresofoneimage = new List<float>();
+            foreach (var image in images)
+            {
+                Bitmap image1 = ResizeBitmap(image, 300, 300);
+                featuresofoneimage = ExtractFeaturOfOneImage(image);
+                featuresofallimages.Add(featuresofoneimage.ToArray());
+            }
+            return featuresofallimages;
+        }
+
+        private void openAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finalfeature();
+        }
+
+        private void multiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finalfeature();
+        }
+
+        public void detect()
+        {
+
+
+            var rect = new Rectangle(0, 0, im.Width, im.Height);
+            var data = im.LockBits(rect, ImageLockMode.ReadWrite, im.PixelFormat);
+            var depth = Bitmap.GetPixelFormatSize(data.PixelFormat) / 8; //bytes per pixel
+
+            var buffer = new byte[data.Width * data.Height * depth];
+
+            //copy pixels to buffer
+            Marshal.Copy(data.Scan0, buffer, 0, buffer.Length);
+
+            System.Threading.Tasks.Parallel.Invoke(
+                () =>
+                {
+                    //upper-left
+                    Process(buffer, 0, 0, data.Width / 2, data.Height / 2, data.Width, depth);
+                },
+                () =>
+                {
+                    //upper-right
+                    Process(buffer, data.Width / 2, 0, data.Width, data.Height / 2, data.Width, depth);
+                },
+                () =>
+                {
+                    //lower-left
+                    Process(buffer, 0, data.Height / 2, data.Width / 2, data.Height, data.Width, depth);
+                },
+                () =>
+                {
+                    //lower-right
+                    Process(buffer, data.Width / 2, data.Height / 2, data.Width, data.Height, data.Width, depth);
+                }
+              
+               
+
+               
+               
+
+            );
+
+            //Copy the buffer back to image
+            Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+
+            im.UnlockBits(data);
+            //  pictureBox2.Image = sampleImage;
+            // dstImg = sampleImage;
+        }
+
+        void Process(byte[] buffer, int x, int y, int endx, int endy, int width, int depth)
+        {
+            for (int i = x; i < endx; i++)
+            {
+                for (int j = y; j < endy; j++)
+                {
+                    var offset = ((j * width) + i) * depth;
+                    var B = buffer[offset + 0];
+                    var G = buffer[offset + 1];
+                    var R = buffer[offset + 2];
+                    var a = Math.Max(R, Math.Max(B, G));
+                    var b = Math.Min(R, Math.Min(B, G));
+                    if (!(((R > 95) && (G > 40) && (B > 20) && ((a - b) > 15) && (Math.Abs(R - G) > 15) && (R > G) && (R > B)) || ((R > 220) && (G > 210) && (B > 170) && ((a - b) > 15) && (Math.Abs(R - G) > 15) && (R > G) && (G > B))))
+                    {
+                        buffer[offset + 0] = buffer[offset + 1] = buffer[offset + 2] = 0;
+                    }
+                    else
+                    {
+                        buffer[offset + 0] = buffer[offset + 1] = buffer[offset + 2] = 255;
+                    }
+                }
+            }
+
+        }
+
+        private void trainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Problem train = Problem.Read(@"F:train.txt");
+            Parameter parameters = new Parameter();
+            parameters.C = 32; parameters.Gamma = 8;
+            model = Training.Train(train, parameters);
+        }
+
+        private void resultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            Problem test = Problem.Read(@"F:test.txt");
+            Prediction.Predict(test, @"F:result.text", model, false);
+
+            string contents = File.ReadAllText(@"F:result.text");
+            Console.WriteLine(contents);
+            int x = Int32.Parse(contents);
+            Char c = (char)(x + 64);
+            MessageBox.Show(c.ToString());
+        }
+    }
+}
